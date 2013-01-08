0ea3aab6ecc2bac6a7e14edbd79c0916d6c4bf90
Ben Hood <0x6e6562@gmail.com>
2013-01-08 17:06:54 +0000

    Ran formatter

--- src/professor/connection_test.go
+++ src/professor/connection_test.go
@@ -1,28 +1,27 @@
 package professor
 
 import (
-	"testing"	
+	"testing"
 )
 
 func TestSetKeyspace(t *testing.T) {
 
 	c, err := Connect("127.0.0.1")
 	defer c.Close()
-	
 
-	if err != nil {				
-		t.Error(err)	
+	if err != nil {
+		t.Error(err)
 	} else {
-		
+
 		result, err := c.Query("use system")
-		
+
 		if err != nil {
-			t.Error(err)			
+			t.Error(err)
 		} else {
 			if result != "system" {
 				t.Errorf("Expected %s as a keyspace but got %s", "system", result)
 			}
 		}
 	}
-	
+
 }
